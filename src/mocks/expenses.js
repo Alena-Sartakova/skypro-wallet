7cf2b9fd7f74a44a589bf0ce13554fd619f57316
Alena <sartakovaalenka@gmail.com>
2025-08-19 01:15:07 +0700

    Базовая стилистика, роутинг, лейаут, форма входа и регистрации, страницы расходов и аналитики

--- src/mocks/expenses.js
+++ src/mocks/expenses.js
@@ -12,5 +12,12 @@ export const mockExpenses = [
     category: 'Транспорт',
     date: '2025-08-16',
     amount: 500
-  }
+  },
+    {
+    id: 3,
+    description: 'Диван',
+    category: 'Жилье',
+    date: '2025-08-15',
+    amount: 7500
+  },
 ];
